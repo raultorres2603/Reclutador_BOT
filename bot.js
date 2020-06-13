9055ea8b6076f29efb04f2aa3d093a87dddc2322
raultorres2603 <55553058+raultorres2603@users.noreply.github.com>
2020-06-13 22:00:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,32 +9,32 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if (msg.channel.id === '720598748001992777' && msg.content.substring(0,5) == 'busco') {
+    if (msg.channel.id === '719920516378657028' && msg.content.substring(0,9) == '!buscoSOT') {
         msg.reply('Anuncio de busqueda de tripulacion creado en el canal de <#721356815643967590>');
         //ID del usuario
         user = msg.author.id;
         //Enviar mensaje a un canal
         //Poner entre corchetes con @ el ID de usuario y con @& si es un rol
         const channel = client.channels.cache.get('721356815643967590');
-        channel.send('Hey! <@' + user + '>' + ' busca ' + msg.content.substring(6) + ' <@&719917491672842281>');
+        channel.send('Hey! <@' + user + '>' + ' busca ' + msg.content.substring(10) + ' <@&719917491672842281>');
 
-    } else if (msg.channel.id === '720599069642457109' && msg.content.substring(0, 5) == 'busco') {
+    } else if (msg.channel.id === '719920516378657028' && msg.content.substring(0, 9) == '!buscoLOL') {
         msg.reply('Anuncio de busqueda de equipo creado en el canal de <#721358528953974835>');
         //ID del usuario
         user = msg.author.id;
         //Enviar mensaje a un canal
         //Poner entre corchetes con @ el ID de usuario y con @& si es un rol
         const channel = client.channels.cache.get('721358528953974835');
-        channel.send('Hey! <@' + user + '>' + ' busca ' + msg.content.substring(6) + ' <@&720254372415668286>');
+        channel.send('Hey! <@' + user + '>' + ' busca ' + msg.content.substring(10) + ' <@&720254372415668286>');
 
-    } else if (msg.channel.id === '720740073301344346' && msg.content.substring(0, 5) == 'busco') {
+    } else if (msg.channel.id === '719920516378657028' && msg.content.substring(0, 9) == '!buscoVAL') {
         msg.reply('Anuncio de busqueda de equipo creado en el canal de <#721408023565697024>');
         //ID del usuario
         user = msg.author.id;
         //Enviar mensaje a un canal
         //Poner entre corchetes con @ el ID de usuario y con @& si es un rol
         const channel = client.channels.cache.get('721408023565697024');
-        channel.send('Hey! <@' + user + '>' + ' busca ' + msg.content.substring(6) + ' <@&719980683430461592>');
+        channel.send('Hey! <@' + user + '>' + ' busca ' + msg.content.substring(10) + ' <@&719980683430461592>');
 
     }
 });
