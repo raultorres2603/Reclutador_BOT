647f919023b18c8040c17445a4b4a97de6121a62
raultorres2603 <55553058+raultorres2603@users.noreply.github.com>
2020-06-13 19:04:21 +0200

    AÑADIR VALORANT

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ client.on('ready', () => {
 
 client.on('message', msg => {
     if (msg.channel.id === '720598748001992777' && msg.content.substring(0,5) == 'busco') {
-        msg.reply('Anuncio de busqueda de tripulacion creado en el canal de #buscar-tripulacion');
+        msg.reply('Anuncio de busqueda de tripulacion creado en el canal de <#721356815643967590>');
         //ID del usuario
         user = msg.author.id;
         //Enviar mensaje a un canal
@@ -19,13 +19,22 @@ client.on('message', msg => {
         channel.send('Hey! <@' + user + '>' + ' busca equipo para ' + msg.content.substring(6) + ' <@&719917491672842281>');
 
     } else if (msg.channel.id === '720599069642457109' && msg.content.substring(0, 5) == 'busco') {
-        msg.reply('Anuncio de busqueda de equipo creado en el canal de #buscar-equipo');
+        msg.reply('Anuncio de busqueda de equipo creado en el canal de <#721358528953974835>');
         //ID del usuario
         user = msg.author.id;
         //Enviar mensaje a un canal
         //Poner entre corchetes con @ el ID de usuario y con @& si es un rol
         const channel = client.channels.cache.get('721358528953974835');
         channel.send('Hey! <@' + user + '>' + ' busca equipo para ' + msg.content.substring(6) + ' <@&720254372415668286>');
+
+    } else if (msg.channel.id === '720740073301344346' && msg.content.substring(0, 5) == 'busco') {
+        msg.reply('Anuncio de busqueda de equipo creado en el canal de <#721408023565697024>');
+        //ID del usuario
+        user = msg.author.id;
+        //Enviar mensaje a un canal
+        //Poner entre corchetes con @ el ID de usuario y con @& si es un rol
+        const channel = client.channels.cache.get('721408023565697024');
+        channel.send('Hey! <@' + user + '>' + ' busca equipo para ' + msg.content.substring(6) + ' <@&719980683430461592>');
 
     }
 });
