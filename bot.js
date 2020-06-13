840c21ff2565bbf69166aafda194c7a8144f8851
raultorres2603 <55553058+raultorres2603@users.noreply.github.com>
2020-06-13 19:18:43 +0200

    4

--- bot.js
+++ bot.js
@@ -16,7 +16,7 @@ client.on('message', msg => {
         //Enviar mensaje a un canal
         //Poner entre corchetes con @ el ID de usuario y con @& si es un rol
         const channel = client.channels.cache.get('721356815643967590');
-        channel.send('Hey! <@' + user + '>' + ' busca tripulacion para ' + msg.content.substring(6) + ' <@&719917491672842281>');
+        channel.send('Hey! <@' + user + '>' + ' busca ' + msg.content.substring(6) + ' <@&719917491672842281>');
 
     } else if (msg.channel.id === '720599069642457109' && msg.content.substring(0, 5) == 'busco') {
         msg.reply('Anuncio de busqueda de equipo creado en el canal de <#721358528953974835>');
@@ -25,7 +25,7 @@ client.on('message', msg => {
         //Enviar mensaje a un canal
         //Poner entre corchetes con @ el ID de usuario y con @& si es un rol
         const channel = client.channels.cache.get('721358528953974835');
-        channel.send('Hey! <@' + user + '>' + ' busca equipo para ' + msg.content.substring(6) + ' <@&720254372415668286>');
+        channel.send('Hey! <@' + user + '>' + ' busca ' + msg.content.substring(6) + ' <@&720254372415668286>');
 
     } else if (msg.channel.id === '720740073301344346' && msg.content.substring(0, 5) == 'busco') {
         msg.reply('Anuncio de busqueda de equipo creado en el canal de <#721408023565697024>');
@@ -34,7 +34,7 @@ client.on('message', msg => {
         //Enviar mensaje a un canal
         //Poner entre corchetes con @ el ID de usuario y con @& si es un rol
         const channel = client.channels.cache.get('721408023565697024');
-        channel.send('Hey! <@' + user + '>' + ' busca equipo para ' + msg.content.substring(6) + ' <@&719980683430461592>');
+        channel.send('Hey! <@' + user + '>' + ' busca ' + msg.content.substring(6) + ' <@&719980683430461592>');
 
     }
 });
