08d9506dcc1f03b31320bfcf9b65ad4d791c5e83
raultorres2603 <55553058+raultorres2603@users.noreply.github.com>
2020-06-13 18:37:11 +0200

    make it better

---
+++ bot.js
@@ -0,0 +1,33 @@
+// Run dotenv
+require('dotenv').config();
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.channel.id === '720598748001992777' && msg.content.substring(0,5) == 'busco') {
+        msg.reply('Anuncio de busqueda de tripulacion creado en el canal de #buscar-tripulacion');
+        //ID del usuario
+        user = msg.author.id;
+        //Enviar mensaje a un canal
+        //Poner entre corchetes con @ el ID de usuario y con @& si es un rol
+        const channel = client.channels.cache.get('721356815643967590');
+        channel.send('Hey! <@' + user + '>' + ' busca equipo para ' + msg.content.substring(6) + ' <@&719917491672842281>');
+
+    } else if (msg.channel.id === '720599069642457109' && msg.content.substring(0, 5) == 'busco') {
+        msg.reply('Anuncio de busqueda de equipo creado en el canal de #buscar-equipo');
+        //ID del usuario
+        user = msg.author.id;
+        //Enviar mensaje a un canal
+        //Poner entre corchetes con @ el ID de usuario y con @& si es un rol
+        const channel = client.channels.cache.get('721358528953974835');
+        channel.send('Hey! <@' + user + '>' + ' busca equipo para ' + msg.content.substring(6) + ' <@&720254372415668286>');
+
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
