7f6c912bb0fe050252371ac7d28e818f5217d3a2
raultorres2603 <55553058+raultorres2603@users.noreply.github.com>
2020-06-15 19:16:34 +0200

    Saber si está en un canal el usuario que pide equipo y postear canal

--- bot.js
+++ bot.js
@@ -11,16 +11,16 @@ client.on('ready', () => {
 client.on('message', msg => {
     // Si estamos en el canal de comandos
     if (msg.channel.id === '719920516378657028') {
-
+        if (msg.member.voice.channel) {
         // Comandos para buscar equipos //
-        
         if (msg.content.substring(0, 9) == '!buscoSOT') {
+            msg.reply('Anuncio de busqueda de tripulación creado en el canal de <#721356815643967590>');
             //ID del usuario
             user = msg.author.id;
             //Enviar mensaje a un canal
             //Poner entre corchetes con @ el ID de usuario y con @& si es un rol
             const channel = client.channels.cache.get('721356815643967590');
-            channel.send('Hey! <@' + user + '>' + ' busca ' + msg.content.substring(10) + ' <@&719917491672842281>');
+            channel.send('Hey! <@' + user + '>' + ' busca ' + msg.content.substring(10) + ' en el canal de voz ' + '<#' + msg.member.voice.channel.id + '>' + ' <@&719917491672842281>');
 
         } else if (msg.content.substring(0, 9) == '!buscoLOL') {
             msg.reply('Anuncio de busqueda de equipo creado en el canal de <#721358528953974835>');
@@ -29,7 +29,7 @@ client.on('message', msg => {
             //Enviar mensaje a un canal
             //Poner entre corchetes con @ el ID de usuario y con @& si es un rol
             const channel = client.channels.cache.get('721358528953974835');
-            channel.send('Hey! <@' + user + '>' + ' busca ' + msg.content.substring(10) + ' <@&720254372415668286>');
+            channel.send('Hey! <@' + user + '>' + ' busca ' + msg.content.substring(10) + ' en el canal de voz ' + '<#' + msg.member.voice.channel.id + '>' + ' <@&720254372415668286>');
 
         } else if (msg.content.substring(0, 9) == '!buscoVAL') {
             msg.reply('Anuncio de busqueda de equipo creado en el canal de <#721408023565697024>');
@@ -38,7 +38,7 @@ client.on('message', msg => {
             //Enviar mensaje a un canal
             //Poner entre corchetes con @ el ID de usuario y con @& si es un rol
             const channel = client.channels.cache.get('721408023565697024');
-            channel.send('Hey! <@' + user + '>' + ' busca ' + msg.content.substring(10) + ' <@&719980683430461592>');
+            channel.send('Hey! <@' + user + '>' + ' busca ' + msg.content.substring(10) + ' en el canal de voz ' + '<#' + msg.member.voice.channel.id + '>' +  ' <@&719980683430461592>');
 
         } else if (msg.content.substring(0, 10) == '!buscoCSGO') {
             msg.reply('Anuncio de busqueda de escuadron creado en el canal de <#721471394671755368>');
@@ -47,11 +47,14 @@ client.on('message', msg => {
             //Enviar mensaje a un canal
             //Poner entre corchetes con @ el ID de usuario y con @& si es un rol
             const channel = client.channels.cache.get('721471394671755368');
-            channel.send('Hey! <@' + user + '>' + ' busca ' + msg.content.substring(11) + ' <@&721470409303654492>');
+            channel.send('Hey! <@' + user + '>' + ' busca ' + msg.content.substring(11) + ' en el canal de voz ' + '<#' + msg.member.voice.channel.id + '>' +  ' <@&721470409303654492>');
         }
+    } else if (!msg.member.voice.channel && !msg.author.bot) {
+        msg.reply('Primero tienes que ir a un canal de voz para usar los comandos de busqueda de equipo!');
+    }
 
         /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-    } else if (msg.channel.id != '719920516378657028' && msg.author.id != '721350612255637505' && msg.content.substring(0,1) == '!') {
+    } else if (msg.channel.id != '719920516378657028' && !msg.author.bot && (msg.content.substring(0,1) == '!' || msg.content.substring(0,1) == '?')) {
         msg.reply('Éste canal no admite comandos, visita <#719920516378657028>');
     } 
 });
