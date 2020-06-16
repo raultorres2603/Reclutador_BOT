37f6e006b98e3681ca581a25ab79d34f79dbe10d
raultorres2603 <55553058+raultorres2603@users.noreply.github.com>
2020-06-16 21:40:57 +0200

    embed

--- bot.js
+++ bot.js
@@ -15,7 +15,7 @@ client.on('message', msg => {
     // Si estamos en el canal de comandos
     if (msg.channel.id === '719920516378657028' && (msg.content.startsWith('!') || msg.content.startsWith('?'))) {
         if (msg.member.voice.channel) {
-            const invite = msg.member.voice.channel.createInvite().then(invite => invite.code);
+            const invite = msg.member.voice.channel.createInvite().then(() => invite.code);
         // Comandos para buscar equipos //
         if (msg.content.startsWith('!buscoSOT')) {
             msg.reply('Anuncio de busqueda de tripulación creado en el canal de <#721356815643967590>');
@@ -67,7 +67,7 @@ client.on('message', msg => {
             const embed = new Discord.MessageEmbed()
             .setColor('#0099ff')
             .setAuthor(client.user.username)
-            .setTitle(invite)
+            .setTitle('Anuncio')
             .setURL('https://discord.gg/' + invite)
             .addField ('Notificacion','Hey! <@' + user + '>' + ' busca ' + msg.member.voice.channel.userLimit + ' soldados')
             .addField('Plan', msg.content.substring(10))
