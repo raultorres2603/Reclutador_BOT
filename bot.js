f8f0e1b4a924f80c467d6a3b195e4f9bb5bb3303
Raul <raultorres2603@gmail.com>
2021-07-14 18:38:46 +0200

    .

--- bot.js
+++ bot.js
@@ -23,7 +23,7 @@ client.on('message', msg => {
 
             if (msg.content.startsWith('!unete')) {
                 // Only try to join the sender's voice channel if they are in one themselves
-                connection = await msg.member.voice.channel.join()
+                connection = await msg.member.voice.channel.join();
                 connection.play(ytdl('https://www.youtube.com/watch?v=ZlAU_w7-Xp8', { filter: 'audioonly' }));
                 if (msg.content.startsWith('!vete')) {
                     msg.member.voice.channel.leave();
