fd0f9683bad2c4503965034b8f177e1b6e92426f
Raul <raultorres2603@gmail.com>
2021-07-15 23:22:46 +0200

    .

--- bot.js
+++ bot.js
@@ -44,7 +44,6 @@ client.on('message', async msg => {
                             posicion_videos = 0;
                             msg.member.voice.channel.leave();
                         } else {
-                            dispatcher = null;
                             posicion_videos++;
                             let stream = ytdl(videos[posicion_videos], { filter: 'audioonly' });
                             dispatcher = connection.play(stream);
