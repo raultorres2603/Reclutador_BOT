21c4b5e6208311058e5a1117a8940ad87d78c630
Raul <raultorres2603@gmail.com>
2021-07-17 23:02:05 +0200

    s

--- bot.js
+++ bot.js
@@ -4,6 +4,16 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const avatar_url = 'https://lh3.googleusercontent.com/proxy/SK3HurdymtUsSf0_L1WDPrheXaWfhjrN47PWdbven_BimSP1jqmBGg1L9qnadsku_TxR51tCXHHLD6fcZC345_uKpuRr1_hzb4RtVr0hpyGiLhduFCBJQRDmwTYCbkiNnDamZMsw3braMKbMLVrYUEQOP7j53a0';
+const ytdl = require('ytdl-core');
+var connection;
+var link;
+let dispatcher;
+var videos = [];
+var YOUTUBE_CRED = `${process.env.YOUTUBE_API}`;
+var YouTube = require("discord-youtube-api");
+let posicion_videos = 0;
+
+var youtube = new YouTube(`${YOUTUBE_CRED}`);
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`);
@@ -12,10 +22,66 @@ client.on('ready', () => {
 
 // NECESIDAD DE CANAL DE COMANDOS ESPECÍFICO
 
-client.on('message', msg => {
+client.on('message', async msg => {
     // Si estamos en el canal de comandos
     if (msg.channel.id === '719920516378657028' && (msg.content.startsWith('!') || msg.content.startsWith('?'))) {
         if (msg.member.voice.channel) {
+            // Unir al bot
+
+            if (msg.content.startsWith('!play')) {
+                // Only try to join the sender's voice channel if they are in one themselves
+                connection = await msg.member.voice.channel.join();
+                link = msg.content.substring(6);
+                let url = await searchYouTubeAsync(link);
+                if (posicion_videos == 0 && videos.length == 0) {
+                    videos.push(url);
+                    let stream = ytdl(url, { filter: 'audioonly' });
+                    dispatcher = connection.play(stream, { volume: '0.5' });
+                    // Hay que poner finish en vez de end, ni puto caso a la documentación oficial
+                    // https://stackoverflow.com/questions/61050918/discord-js-bot-unable-to-leave-voice-channel
+                    dispatcher.on('finish', () => {
+                        if (posicion_videos + 1 == videos.length) {
+                            dispatcher = undefined;
+                            videos = [];
+                            posicion_videos = 0;
+                            msg.member.voice.channel.leave();
+                        } else {
+                            nextSong(msg);
+                        }
+                    })
+                    msg.reply(`Se ha añadido tu canción y se reproducirá ahora: ${url}`);
+                } else {
+                    videos.push(url);
+                    msg.reply(`Se ha añadido tu canción: ${url}`);
+                }
+                /*
+                let stream = ytdl(url, { filter: 'audioonly' });
+                dispatcher = connection.play(stream);
+                */
+
+            }
+            if (msg.content.startsWith('!leave')) {
+                if (typeof dispatcher != undefined) {
+                    dispatcher = undefined;
+                    videos = [];
+                    posicion_videos = 0;
+                }
+                msg.member.voice.channel.leave();
+            } if (msg.content.startsWith('!stop')) {
+                dispatcher = undefined;
+                videos = [];
+                posicion_videos = 0;
+            }
+
+            if (msg.content.startsWith('!skip')) {
+                if (posicion_videos + 1 == videos.length) {
+                    msg.reply("No hay más canciones, esta es la última.");
+                } else {
+                    nextSong(msg);
+                    msg.reply(`Se ha pasado a la siguiente canción: ${videos[posicion_videos]}`);
+                }
+            }
+
             const invite = msg.member.voice.channel.createInvite()
                 .then(invite => {
                     if (msg.content.startsWith('!buscoSOT')) {
@@ -106,5 +172,30 @@ client.on('message', msg => {
         msg.reply('Éste canal no admite comandos, visita <#719920516378657028>');
     }
 });
+
+async function searchYouTubeAsync(args) {
+    var video = await youtube.searchVideos(args.toString().replace(/,/g, ' '));
+    console.log(video.url);
+    console.log(typeof String(video.url));
+    return String(video.url);
+}
+
+function nextSong(msg) {
+    posicion_videos = posicion_videos + 1;
+    let stream = ytdl(videos[posicion_videos], { filter: 'audioonly' });
+    dispatcher = connection.play(stream, { volume: '0.5' });
+    dispatcher.on('finish', () => {
+        if (posicion_videos + 1 >= videos.length) {
+            dispatcher = undefined;
+            videos = [];
+            posicion_videos = 0;
+            msg.member.voice.channel.leave();
+        } else {
+            nextSong(msg);
+        }
+    })
+    console.log(dispatcher);
+    return dispatcher;
+}
 
 client.login(process.env.DISCORD_TOKEN);
